709d3f80e3a6184f6106c874c5c0d1dc512c13e9
aniyah <ybf3jw@virginia.edu>
2024-03-20 22:01:45 -0400

    latest version of lab 4

--- mywork/pythonurl.py
+++ mywork/pythonurl.py
@@ -3,18 +3,21 @@
 import boto3
 s3=boto3.client('s3')
 
-#vars needed
-bucket_name = 'ybf3jw'
-object_name = 'imagereq'
-expires_in = 604800
-
 #creating the function to save a file from the interent using requests
 # i am lowkey not sure how I would incorporate this but I just wanted to make sure it didnt require the user to manually download the image
 import urllib.request
 
 def url(url, imageName):
 	urllib.request.urlretrieve(url,imageName)
-url('https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/7298946f2a94eecc85eebf257a8877c7.jpg', 'legendOfZelda')
+
+url('https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/7298946f2a94eecc85eebf257a8877c7.jpg', 'legendOfZelda.jpg')
+
+#vars needed
+bucket_name = 'ds2002-ybf3jw'
+object_name = 'legendOfZelda.jpg'
+expires_in = 604800
+
+
 def presignedurl(bucket_name, object_name, expires_in=604800):
 	response=s3.generate_presigned_url(
 	'get_object',
@@ -22,7 +25,7 @@ def presignedurl(bucket_name, object_name, expires_in=604800):
 	)
 	return response
 
-print(presignedurl(bucket_name,'legendOfZelda',expires_in))
+print(presignedurl(bucket_name,'legendOfZelda.jpg',expires_in))
 
 
 
